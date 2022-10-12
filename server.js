7fcfe7590fab4b34344f70a4db0938a357fa4356
Fatih Emre <108624615+fatihemre01@users.noreply.github.com>
2022-10-12 21:45:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-require("dotenv").config()
-const cookieParser = require("cookie-parser")
-const express = require("express")
-const mongoose = require("mongoose")
-const authRoute = require("./routes/authRoute")
-const blogRoute = require("./routes/blogRoute")
-const {checkUser} = require("./middlewares/authMiddle")
+require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/authRoute");
+const blogRoute = require("./routes/blogRoute");
+const { checkUser } = require("./middlewares/authMiddle");
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-const app = express()
+const app = express();
 
-app.set("search-engine","ejs")
+app.set("search-engine", "ejs");
 
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-mongoose.connect("mongodb://localhost/BlogDB")
-.then(() => console.log("Connected"), app.listen(port))
-.catch(() => console.log("Not connected"))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected"))
+  .catch(() => console.log("Not connected"));
 
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("home.ejs"));
 
-app.get("*", checkUser)
-app.get("/", (req, res) => res.render("home.ejs"))
+app.use(authRoute);
+app.use(blogRoute);
 
-app.use(authRoute)
-app.use(blogRoute)
+app.listen(port, () => console.log("Running on", port));
